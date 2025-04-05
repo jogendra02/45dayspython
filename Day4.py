ce54b8ddbee8d952210ddb40e31c61ba68b15840
jogendra02 <jogendrakumarich207@gmail.com>
2025-04-05 16:35:36 +0530

    day4.py file is updated

---
+++ Day4.py
@@ -0,0 +1,6 @@
+thislist =['apple', 'mango', 'guava', 'watermelon'];
+print(thislist);
+thismob = ['vivo', 'realme' 'oppo', 'iphone'];
+print(thismob)
+thismodel = ['activa', 'pleasure', 'tvs jupitor'];
+print(thismodel)
