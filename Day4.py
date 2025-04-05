a1c1e5b2c4e8bb0bb35050755d5b1ef0f106728b
jogendra02 <jogendrakumarich207@gmail.com>
2025-04-05 19:08:15 +0530

    day4.py file is updated

--- Day4.py
+++ Day4.py
@@ -4,3 +4,12 @@ thismob = ['vivo', 'realme' 'oppo', 'iphone'];
 print(thismob)
 thismodel = ['activa', 'pleasure', 'tvs jupitor'];
 print(thismodel)
+thisstyle = ["border", "color", "padding", "height", "width"];
+thisstyle[3] = "margin";
+print(thisstyle)
+thiscombo = ["shirt", "jeans", "shoes"];
+thiscombo.append('sweatshirts')
+print(thiscombo)
+thispair = ["slippers", "teddybear", "mouses", "systems"];
+thispair.remove("mouses")
+print(thispair)
