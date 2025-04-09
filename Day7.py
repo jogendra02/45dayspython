3e40cccd55512b0e291db06211672d3ff9f139c2
jogendra02 <jogendrakumarich207@gmail.com>
2025-04-09 16:53:15 +0530

    Day7.py file is updated

---
+++ Day7.py
@@ -0,0 +1,7 @@
+thistuple =('carrot', 'beetroot', 'cabbage', 'domestick', 'onion');
+if 'cabbage' in thistuple:
+    print("yes cabbage is in thistuple")
+    thistuple =('apple', 'orange', 'watermelon', 'banana');
+    y =list(thistuple);
+    y.append('papaya');
+    print(y)
