8358a9549f938d76a19bfe3988b38e7651cb951c
jogendra02 <jogendrakumarich207@gmail.com>
2025-04-09 16:44:01 +0530

    Day5.py file is updated

---
+++ Day6.py
@@ -0,0 +1,8 @@
+thistuple =('apple', 'mango', 'banana', 'cherry');
+print(thistuple)
+thistuple =('tiger', 'elephant', 'zibra', 'kangarao');
+print(thistuple[2])
+thistuple =('meter', 'centimeter', 'kilogram', 'gram');
+print(thistuple[1:2])
+thistuple =('milk', 'coffee', 'tea', 'badam shake');
+print(thistuple[:3])
