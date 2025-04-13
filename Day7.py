babc411fd97808b7ba7d932fac02e04550e80972
jogendra02 <jogendrakumarich207@gmail.com>
2025-04-13 22:49:31 +0530

    Day7.py file is updated

--- Day7.py
+++ Day7.py
@@ -1,7 +1,7 @@
 thistuple =('carrot', 'beetroot', 'cabbage', 'domestick', 'onion');
 if 'cabbage' in thistuple:
     print("yes cabbage is in thistuple")
-    thistuple =('apple', 'orange', 'watermelon', 'banana');
+    thistuple =('apple', 'orange', 'watermelon', 'banana', 'grapes');
     y =list(thistuple);
     y.append('papaya');
     print(y)
