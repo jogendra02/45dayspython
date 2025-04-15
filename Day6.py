becaa8167e74a3cdf09629b395cf9c8b7ad5609b
jogendra02 <jogendrakumarich207@gmail.com>
2025-04-15 21:56:58 +0530

    Day6.py file is updated

--- Day6.py
+++ Day6.py
@@ -4,5 +4,5 @@ thistuple =('tiger', 'elephant', 'zibra', 'kangarao');
 print(thistuple[2])
 thistuple =('meter', 'centimeter', 'kilogram', 'gram');
 print(thistuple[1:2])
-thistuple =('milk', 'coffee', 'tea', 'badam shake');
+thistuple =('milk', 'coffee', 'tea', 'badam shake', "almond milk");
 print(thistuple[:3])
